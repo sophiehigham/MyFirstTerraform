bf64db3827287783370e619eed16e9e673d312e8
Sophie Higham <Sophie.Higham@bailliegifford.com>
2025-03-28 15:06:39 +0000

    add deployment

--- providers.tf
+++ providers.tf
@@ -3,3 +3,10 @@ provider "azurerm" {
   subscription_id                 = "da8e9b40-8876-4ee7-98e3-b854c7a9b2d2"
   resource_provider_registrations = "none"
 }
+
+provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.cluster_ca_certificate)
+}
